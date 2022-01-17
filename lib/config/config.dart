16b4f17bba6676e9db37f2c1f855823e0a0cfcc1
muflikhandimasd <muflikhan.dimas@gmail.com>
2022-01-18 05:17:00 +0700

    Add Guide Local API

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,6 @@
 class Config {
   static const String appName = "Shopping App";
+  // API url is local IP => IPv4 address of wifi
   static const String apiURL = "192.168.8.181:4000";
   static const String productURL = "/api/products";
 }
